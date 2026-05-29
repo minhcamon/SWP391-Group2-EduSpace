5336c3e17459ddb7334d255b64d859951dd6c69a
Nguyễn Minh Hiếu <hieumastermind@gmail.com>
2026-05-30 00:15:27 +0700

    feat:add base course entity, repository and service

---
+++ backend/src/main/java/org/eduspace/backend/entity/ClassTimeline.java
@@ -0,0 +1,31 @@
+package org.eduspace.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "timelines")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClassTimeline {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "timeline_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "module_id")
+    private Module module;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_id")
+    private CourseClass courseClass;
+
+    @Column(name = "due_date")
+    private LocalDateTime dueDate;
+}
---
+++ backend/src/main/java/org/eduspace/backend/entity/Module.java
@@ -0,0 +1,42 @@
+package org.eduspace.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.eduspace.backend.enums.ModulePriority;
+
+@Data
+@Entity
+@Table(name = "modules")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Module {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "module_id")
+    private Long id;
+
+    @Column(name = "title", nullable = false, columnDefinition = "TEXT")
+    private String title;
+
+    @Column(name = "priority")
+    @Enumerated(EnumType.STRING)
+    private ModulePriority priority;
+
+    @Column(name = "days")
+    private int days;
+
+    @Column(name = "base_exp")
+    private int baseExp;
+
+    @Column(name = "speed_bonus_exp")
+    private int speedBonusExp;
+
+    @Column(name = "sort_order")
+    private int sortOrder;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+}
