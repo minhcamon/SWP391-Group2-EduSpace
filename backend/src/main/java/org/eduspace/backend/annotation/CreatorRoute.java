86d73ee097c8d695aed4ae27132f0a5c6a082f65
Nguyễn Minh Hiếu <hieumastermind@gmail.com>
2026-05-30 18:43:39 +0700

    feat(be): add annotation package
    - Custom prefix path for different roles to avoid repeating and easily use at controller

---
+++ backend/src/main/java/org/eduspace/backend/annotation/CreatorRoute.java
@@ -0,0 +1,13 @@
+package org.eduspace.backend.annotation;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@RequestMapping("/api/creator")
+public @interface CreatorRoute {
+    String value() default "";
+}
