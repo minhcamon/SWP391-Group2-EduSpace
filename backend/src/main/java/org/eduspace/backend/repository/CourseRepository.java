d18cbfc3ee39b6f0ecb5a5cef931c3ba2eec708a
MonaLisa <ducngoc14102003@gmail.com>
2026-05-31 22:21:19 +0700

    add AdminCourseController

--- backend/src/main/java/org/eduspace/backend/repository/CourseRepository.java
+++ backend/src/main/java/org/eduspace/backend/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package org.eduspace.backend.repository;
 
 import org.eduspace.backend.entity.Course;
+import org.eduspace.backend.enums.CourseStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +13,5 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> getCoursesByCreatorId(@Param("creatorId") Long creatorId);
 
     List<Course> findByIsDeletedFalse();
+    List<Course> findByStatusAndIsDeletedFalse(CourseStatus status);
 }
