a1d94e818844d7befa62c4fc3883bbf8feedfb00
capyboiii <voquocbao2k6@gmail.com>
2026-05-31 17:53:03 +0700

    fix(be): published courses endpoint for learner

--- backend/src/main/java/org/eduspace/backend/controller/UserController.java
+++ backend/src/main/java/org/eduspace/backend/controller/UserController.java
@@ -5,9 +5,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
-import java.util.List;
-
 import org.eduspace.backend.dto.response.APIResponse;
 import org.eduspace.backend.dto.response.CourseResponse;
 import org.eduspace.backend.dto.response.UserResponse;
@@ -27,7 +24,6 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "User", description = "Các API quản lý và lấy thông tin người dùng")
 public class UserController {
     private final AuthService authService;
-    private final CourseService courseService;
 
     @Operation(summary = "Lấy thông tin cá nhân (Profile)", description = "Lấy thông tin chi tiết của người dùng hiện tại từ JWT token đã xác thực.")
     @ApiResponses(value = {
@@ -43,17 +39,4 @@ public class UserController {
                 APIResponse.success("Successfull Get Profile", userResponse));
     }
 
-        @Operation(summary = "Lấy danh sách khóa học", description = "Lấy tất cả khóa học có status = PUBLISHED và chưa bị xóa.")
-        @ApiResponses(value = {
-                @ApiResponse(responseCode = "200", description = "Lấy danh sách khóa học thành công"),
-                @ApiResponse(responseCode = "401", description = "Chưa xác thực hoặc token không hợp lệ"),
-                @ApiResponse(responseCode = "403", description = "Tài khoản bị cấm hoặc không có quyền truy cập")
-        })
-        @SecurityRequirement(name = "Bearer Authentication")
-        @GetMapping("/courses")
-        public ResponseEntity<APIResponse<List<CourseResponse>>> getAllPublishedCourses() {
-                List<CourseResponse> courses = courseService.getAllPublishedCourses();
-                return ResponseEntity.ok(
-                        APIResponse.success("Successfully fetched courses", courses));
-        }
 }
