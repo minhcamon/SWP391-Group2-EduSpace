b81a896640f7edcc73919a3e2303d7d93c93b1db
Nguyễn Minh Hiếu <hieumastermind@gmail.com>
2026-06-01 19:42:09 +0700

    chore(be): bypass jwt validation for new public endpoint
    - Add api/course/all to bypassw jwt validation

--- backend/src/main/java/org/eduspace/backend/security/AuthenticationFilter.java
+++ backend/src/main/java/org/eduspace/backend/security/AuthenticationFilter.java
@@ -20,34 +20,38 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class AuthenticationFilter extends OncePerRequestFilter {
     private final UserRepository userRepository;
+
     // 1. Loại bỏ các API Public khỏi Filter này
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String path = request.getServletPath();
         return path.startsWith("/api/auth/")
+                || path.startsWith("/api/course/all")
                 || path.startsWith("/swagger-ui")
                 || path.startsWith("/v3/api-docs");
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication != null && authentication.getPrincipal() instanceof Jwt jwt){
+        if (authentication != null && authentication.getPrincipal() instanceof Jwt jwt) {
             Object userIdObj = jwt.getClaim("userId");
             Long userId = null;
             if (userIdObj instanceof Number number) {
                 userId = number.longValue();
-            }else if(userIdObj instanceof String str) {
+            } else if (userIdObj instanceof String str) {
                 userId = Long.parseLong(str);
             }
 
-            if(userId != null){
+            if (userId != null) {
                 User user = userRepository.findById(userId).orElse(null);
 
                 if (user != null && user.getStatus() != UserStatus.ACTIVE) {
                     response.setStatus(HttpServletResponse.SC_FORBIDDEN);
                     response.setContentType("application/json;charset=UTF-8");
-                    response.getWriter().write("{\"code\": 403, \"message\": \"Your account has been banned or disabled!\"}");
+                    response.getWriter()
+                            .write("{\"code\": 403, \"message\": \"Your account has been banned or disabled!\"}");
                     return;
                 }
             }
