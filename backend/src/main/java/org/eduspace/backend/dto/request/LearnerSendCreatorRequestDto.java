e1a27085b85c8c405773364c63e541a2b7aabed5
MonaLisa <ducngoc14102003@gmail.com>
2026-06-01 22:25:17 +0700

    api for learners to submit creator requests

---
+++ backend/src/main/java/org/eduspace/backend/dto/request/LearnerSendCreatorRequestDto.java
@@ -0,0 +1,12 @@
+package org.eduspace.backend.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LearnerSendCreatorRequestDto {
+    @NotBlank(message = "Lý do và kinh nghiệm không được để trống")
+    private String reason;
+}
