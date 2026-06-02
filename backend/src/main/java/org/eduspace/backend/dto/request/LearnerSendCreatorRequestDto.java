4ffc3ec9a174dd53453784f5d3a20d0f1e445861
Nguyễn Minh Hiếu <hieumastermind@gmail.com>
2026-06-03 01:34:00 +0700

    chore(be): remove attribute reason (string) when request to role up

--- backend/src/main/java/org/eduspace/backend/dto/request/LearnerSendCreatorRequestDto.java
+++ backend/src/main/java/org/eduspace/backend/dto/request/LearnerSendCreatorRequestDto.java
@@ -7,6 +7,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LearnerSendCreatorRequestDto {
-    @NotBlank(message = "Lý do và kinh nghiệm không được để trống")
-    private String reason;
+    // @NotBlank(message = "Lý do và kinh nghiệm không được để trống")
+    // private String reason;
 }
