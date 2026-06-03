cd701f0da80bdbfafac63b54315704d0104a8749
MonaLisa <doducngoc14102003@gmail.com>
2026-06-03 17:56:24 +0700

    fix(be): add admin_id, reason and update course status to REJECTED

---
+++ backend/src/main/java/org/eduspace/backend/dto/request/AdminRejectCourseRequest.java
@@ -0,0 +1,20 @@
+package org.eduspace.backend.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminRejectCourseRequest {
+
+    @NotNull(message = "Admin ID cannot be null")
+    private Long adminId; 
+    @NotBlank(message = "Rejection reason cannot be blank")
+    private String reason;
+}
