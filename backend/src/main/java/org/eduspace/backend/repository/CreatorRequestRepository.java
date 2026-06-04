9b9161095409da3230a439b229312fc87aca8eb5
Nguyễn Minh Hiếu <hieumastermind@gmail.com>
2026-06-05 00:03:44 +0700

    chore(be): add entity CreatorRequest
    - This table store information of course's handle by admin with reason if being rejected
    - Change enum CreatorRequestStatus -> RequestStatus for common using with all request

--- backend/src/main/java/org/eduspace/backend/repository/CreatorRequestRepository.java
+++ backend/src/main/java/org/eduspace/backend/repository/CreatorRequestRepository.java
@@ -1,7 +1,7 @@
 package org.eduspace.backend.repository;
 
 import org.eduspace.backend.entity.CreatorRequest;
-import org.eduspace.backend.enums.CreatorRequestStatus;
+import org.eduspace.backend.enums.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.eduspace.backend.entity.User;
@@ -11,8 +11,9 @@ import java.util.Optional;
 
 @Repository
 public interface CreatorRequestRepository extends JpaRepository<CreatorRequest, Long> {
-    List<CreatorRequest> findByStatus(CreatorRequestStatus status);
+    List<CreatorRequest> findByStatus(RequestStatus status);
 
     Optional<CreatorRequest> findById(Long id);
-    boolean existsByLearnerAndStatus(User learner, CreatorRequestStatus status);
+
+    boolean existsByLearnerAndStatus(User learner, RequestStatus status);
 }
--- backend/src/main/java/org/eduspace/backend/service/CreatorRequestService.java
+++ backend/src/main/java/org/eduspace/backend/service/CreatorRequestService.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.eduspace.backend.dto.creator_request.request.CreatorRequestApprovalRequest;
 import org.eduspace.backend.dto.creator_request.response.CreatorRequestApprovalResponse;
 import org.eduspace.backend.entity.CreatorRequest;
-import org.eduspace.backend.enums.CreatorRequestStatus;
+import org.eduspace.backend.enums.RequestStatus;
 import org.eduspace.backend.enums.Role;
 import org.eduspace.backend.repository.CreatorRequestRepository;
 import org.eduspace.backend.repository.UserRepository;
@@ -19,86 +19,91 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class CreatorRequestService {
-    private final CreatorRequestRepository creatorRequestRepository;
-    private final UserRepository userRepository;
+        private final CreatorRequestRepository creatorRequestRepository;
+        private final UserRepository userRepository;
 
-    public List<CreatorRequestApprovalRequest> getAllRequestPending() {
-        List<CreatorRequest> requests = creatorRequestRepository.findByStatus(CreatorRequestStatus.PENDING);
+        public List<CreatorRequestApprovalRequest> getAllRequestPending() {
+                List<CreatorRequest> requests = creatorRequestRepository.findByStatus(RequestStatus.PENDING);
 
-        return requests.stream()
-                .map(request -> CreatorRequestApprovalRequest.builder()
-                        .requestId(request.getId())
-                        .learnerId(request.getLearner().getId())
-                        .learnerName(request.getLearner().getFullName())
-                        .learnerEmail(request.getLearner().getEmail())
-                        .documentUrl(request.getDocumentUrl())
-                        .status(request.getStatus().name())
-                        .build())
-                .collect(Collectors.toList());
+                return requests.stream()
+                                .map(request -> CreatorRequestApprovalRequest.builder()
+                                                .requestId(request.getId())
+                                                .learnerId(request.getLearner().getId())
+                                                .learnerName(request.getLearner().getFullName())
+                                                .learnerEmail(request.getLearner().getEmail())
+                                                .documentUrl(request.getDocumentUrl())
+                                                .status(request.getStatus().name())
+                                                .build())
+                                .collect(Collectors.toList());
 
-    }
-
-    @Transactional
-    public CreatorRequestApprovalResponse approveLearnerToCreator(Long requestId, Long adminId) {
-        CreatorRequest request = creatorRequestRepository.findById(requestId)
-                .orElseThrow(() -> new RuntimeException("Creator request not found with ID: " + requestId));
-
-        if (request.getStatus() != CreatorRequestStatus.PENDING) {
-            throw new RuntimeException("This request has already been processed");
-        }
-        request.setStatus(CreatorRequestStatus.APPROVED);
-        request.getLearner().setRole(Role.CREATOR);
-
-        CreatorRequest updatedRequest = creatorRequestRepository.save(request);
-
-        return CreatorRequestApprovalResponse.builder()
-                .id(updatedRequest.getId())
-                .status(updatedRequest.getStatus().name())
-                .learnerId(updatedRequest.getLearner() != null ? updatedRequest.getLearner().getId() : null)
-                .approvedBy(adminId)
-                .processedAt(LocalDateTime.now())
-                .build();
-    }
-
-    @Transactional
-    public CreatorRequestApprovalResponse rejectLearnerToCreator(Long requestId, Long adminId) {
-        CreatorRequest request = creatorRequestRepository.findById(requestId)
-                .orElseThrow(() -> new RuntimeException("Creator request not found with ID: " + requestId));
-
-        if (request.getStatus() != CreatorRequestStatus.PENDING) {
-            throw new RuntimeException("This request has already been processed");
-        }
-        request.setStatus(CreatorRequestStatus.REJECTED);
-
-        CreatorRequest updatedRequest = creatorRequestRepository.save(request);
-
-        return CreatorRequestApprovalResponse.builder()
-                .id(updatedRequest.getId())
-                .status(updatedRequest.getStatus().name())
-                .learnerId(updatedRequest.getLearner() != null ? updatedRequest.getLearner().getId() : null)
-                .approvedBy(adminId)
-                .processedAt(LocalDateTime.now())
-                .build();
-    }
-
-    @Transactional
-    public void createCreatorRequest(Long learnerId) {
-
-        User learner = userRepository.findById(learnerId)
-                .orElseThrow(() -> new RuntimeException("User (Learner) not found with ID: " + learnerId));
-
-        boolean hasPendingRequest = creatorRequestRepository.existsByLearnerAndStatus(learner,
-                CreatorRequestStatus.PENDING);
-        if (hasPendingRequest) {
-            throw new RuntimeException("Bạn đã có một yêu cầu đang chờ duyệt. Không thể gửi thêm!");
         }
 
-        CreatorRequest newRequest = CreatorRequest.builder()
-                .learner(learner)
-                .status(CreatorRequestStatus.PENDING)
-                // .documentUrl(requestDto.getReason())
-                .build();
+        @Transactional
+        public CreatorRequestApprovalResponse approveLearnerToCreator(Long requestId, Long adminId) {
+                CreatorRequest request = creatorRequestRepository.findById(requestId)
+                                .orElseThrow(() -> new RuntimeException(
+                                                "Creator request not found with ID: " + requestId));
 
-        creatorRequestRepository.save(newRequest);
-    }
+                if (request.getStatus() != RequestStatus.PENDING) {
+                        throw new RuntimeException("This request has already been processed");
+                }
+                request.setStatus(RequestStatus.APPROVED);
+                request.getLearner().setRole(Role.CREATOR);
+
+                CreatorRequest updatedRequest = creatorRequestRepository.save(request);
+
+                return CreatorRequestApprovalResponse.builder()
+                                .id(updatedRequest.getId())
+                                .status(updatedRequest.getStatus().name())
+                                .learnerId(updatedRequest.getLearner() != null ? updatedRequest.getLearner().getId()
+                                                : null)
+                                .approvedBy(adminId)
+                                .processedAt(LocalDateTime.now())
+                                .build();
+        }
+
+        @Transactional
+        public CreatorRequestApprovalResponse rejectLearnerToCreator(Long requestId, Long adminId) {
+                CreatorRequest request = creatorRequestRepository.findById(requestId)
+                                .orElseThrow(() -> new RuntimeException(
+                                                "Creator request not found with ID: " + requestId));
+
+                if (request.getStatus() != RequestStatus.PENDING) {
+                        throw new RuntimeException("This request has already been processed");
+                }
+                request.setStatus(RequestStatus.REJECTED);
+
+                CreatorRequest updatedRequest = creatorRequestRepository.save(request);
+
+                return CreatorRequestApprovalResponse.builder()
+                                .id(updatedRequest.getId())
+                                .status(updatedRequest.getStatus().name())
+                                .learnerId(updatedRequest.getLearner() != null ? updatedRequest.getLearner().getId()
+                                                : null)
+                                .approvedBy(adminId)
+                                .processedAt(LocalDateTime.now())
+                                .build();
+        }
+
+        @Transactional
+        public void createCreatorRequest(Long learnerId) {
+
+                User learner = userRepository.findById(learnerId)
+                                .orElseThrow(() -> new RuntimeException(
+                                                "User (Learner) not found with ID: " + learnerId));
+
+                boolean hasPendingRequest = creatorRequestRepository.existsByLearnerAndStatus(learner,
+                                RequestStatus.PENDING);
+                if (hasPendingRequest) {
+                        throw new RuntimeException("Bạn đã có một yêu cầu đang chờ duyệt. Không thể gửi thêm!");
+                }
+
+                CreatorRequest newRequest = CreatorRequest.builder()
+                                .learner(learner)
+                                .status(RequestStatus.PENDING)
+                                // .documentUrl(requestDto.getReason())
+                                .build();
+
+                creatorRequestRepository.save(newRequest);
+        }
 }
