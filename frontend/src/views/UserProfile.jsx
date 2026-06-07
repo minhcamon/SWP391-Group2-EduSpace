0a1931804eb1cdf2756a6b6de0f857beddeaefac
Minh <nhatminhyt06@gmail.com>
2026-06-08 00:31:26 +0700

    fix(fe): wrong import at App.jsx

---
+++ frontend/src/views/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserProfilePage from "@/modules/shared-features/pages/UserProfilePage";
+import Header from "@/components/layouts/Header";
+const UserProfile = () => {
+    return (
+        <div>
+            <Header />
+            <UserProfilePage />
+        </div>
+    );
+};
+
+export default UserProfile;
