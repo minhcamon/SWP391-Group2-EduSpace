e1d82ed9f0d3995c11d953254aafe3ae58e4eb45
Minh <nhatminhyt06@gmail.com>
2026-07-10 02:45:50 +0700

    fix: undo bell icon for noti

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -63,12 +63,12 @@ const Header = () => {
           {/* Right Action Section */}
           <div className="flex items-center gap-3">
             {/* Notification Bell (Only if Logged In) */}
-            {/* {user && (
-                            <button className="relative p-1.5 text-neutral-medium hover:text-primary rounded-full hover:bg-slate-50 transition-all duration-200 cursor-pointer">
-                                <Bell size={22} />
-                                <span className="absolute top-1.5 right-1.5 w-2 h-2 bg-secondary rounded-full"></span>
-                            </button>
-                        )} */}
+            {user && (
+              <button className="relative p-1.5 text-neutral-medium hover:text-primary rounded-full hover:bg-slate-50 transition-all duration-200 cursor-pointer">
+                <Bell size={22} />
+                <span className="absolute top-1.5 right-1.5 w-2 h-2 bg-secondary rounded-full"></span>
+              </button>
+            )}
 
             {/* User Profile Dropdown or Authentication Buttons */}
             {user ? (
